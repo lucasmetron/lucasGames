a1482b4243773509add0f7e374cc16a14eaa9d99
lucasmetron <lucasmetron@gmail.com>
2022-07-06 17:28:35 -0300

    teste

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,5 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  teste: 'te'
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
