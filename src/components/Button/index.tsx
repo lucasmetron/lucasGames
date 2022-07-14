f0009825316c9db23cd066e605232b34b65a83e1
lucasmetron <lucasmetron@gmail.com>
2022-07-14 17:16:24 -0300

    plop

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as S from './styles'
+
+const Button = () => (
+  <S.Wrapper>
+    <h1>Button</h1>
+  </S.Wrapper>
+)
+
+export default Button
---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button
+} as Meta
+
+export const Default: Story = () => <Button />
